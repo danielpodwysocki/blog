b61d93703584be1331277cf26f2aa18c7a867482
Daniel Podwysocki <daniel@dapo.tech>
2022-03-15 23:24:49 +0100

    feat: fill in the site owner info, social links, etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,13 @@
 module.exports = {
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
+    title: `dapo.tech`,
+    author: `Daniel Podwysocki`,
     description: `My site description...`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        name: `GitHub`,
+        url: `https://github.com/danielpodwysocki`,
       },
     ],
   },
